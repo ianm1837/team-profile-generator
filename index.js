c92b6e28bd43927b8d043b0a781d778e27e650af
ianm1837 <ian@iwm.dev>
2023-01-14 13:04:23 -0800

    fixed exports syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { buildTeam } = require('./lib/buildTeam.js');
-const { buildHTML } = require('./lib/buildHTML.js');
-const { writeToFile } = require('./lib/writeToFile.js');
+const { buildTeam } = require('./lib/functions/buildTeam');
+const { buildHTML } = require('./lib/functions/createHTML');
+const { writeToFile } = require('./lib/functions/writeToFile');
 
 const generatedFileName = path.join(__dirname, 'dist', 'team-info.html');
 
